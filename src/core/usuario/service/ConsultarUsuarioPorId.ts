f22ac496c5940eb8f053a743f38bfb315dd76134
André Miranda <aegis.andre@gmail.com>
2023-09-23 14:17:15 -0300

    clean architecture example

---
+++ src/core/usuario/service/ConsultarUsuarioPorId.ts
@@ -0,0 +1,13 @@
+import CasoDeUso from "../../shared/CasoDeUso"
+import Usuario from "../model/Usuario"
+import RepositorioUsuario from "./RepositorioUsuario"
+
+export default class ConsultarUsuarioPorId
+  implements CasoDeUso<number, Usuario | null>
+{
+  constructor(private readonly repositorio: RepositorioUsuario) {}
+
+  async executar(id: number): Promise<Usuario | null> {
+    return await this.repositorio.consultarPorId(id)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
-import { Elysia } from "elysia";
+import ConsultarUsuarioPorIdController from "./adapters/ConsultarUsuarioPorIdController"
+import ConsultarUsuariosController from "./adapters/ConsultarUsuariosController"
+import RegistrarUsuarioController from "./adapters/RegistrarUsuarioController"
+import ConsultarUsuarioPorId from "./core/usuario/service/ConsultarUsuarioPorId"
+import ConsultarUsuarios from "./core/usuario/service/ConsultarUsuarios"
+import RegistrarUsuario from "./core/usuario/service/RegistrarUsuario"
+import app from "./external/api/config"
+import RepositorioUsuarioMemoria from "./external/memoria/RepositorioUsuarioMemoria"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// -------------------------Registrar rotas------------------------------------------
+
+const repositorioUsuario = new RepositorioUsuarioMemoria()
+const registrarUsuario = new RegistrarUsuario(repositorioUsuario)
+new RegistrarUsuarioController(app, registrarUsuario)
+
+const consultarUsuarios = new ConsultarUsuarios(repositorioUsuario)
+new ConsultarUsuariosController(app, consultarUsuarios)
+
+const consultarUsuarioPorId = new ConsultarUsuarioPorId(repositorioUsuario)
+new ConsultarUsuarioPorIdController(app, consultarUsuarioPorId)
+
+app.listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
